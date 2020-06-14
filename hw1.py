514a708ede5a3da448d2eb98190d17b69f3fe482
Vlad Stanescu <vlad.stanescu@ebs.msft>
2020-06-15 00:22:17 +0300

    Homework1

---
+++ hw1.py
@@ -0,0 +1,16 @@
+# 1. Reverse the order of the items in an array.
+# Example:
+a1 = [1, 2, 3, 4, 5]
+a1 = a1[::-1]
+print(a1)
+
+# 2. Get the number of occurrences of var b in array a.
+a2 = [1, 1, 2, 2, 2, 2, 3, 3, 3]
+b2 = 2
+print(a2.count(b2))
+
+# 3. Given a sentence as string, count the number of words in it.
+
+a3 = 'ana are mere si nu are pere'
+a3_list = a3.split(" ")
+print(len(a3_list))
